de620f0efd29d5e0bcf5137ae456fdc23fef4bad
Manuel Eder <83589343+ManuelEderr@users.noreply.github.com>
2022-09-21 09:15:09 +0200

    Playfield

--- src/main/java/htl/steyr/snake/Controller/PlayfieldController.java
+++ src/main/java/htl/steyr/snake/Controller/PlayfieldController.java
@@ -1,4 +1,10 @@
 package htl.steyr.snake.Controller;
 
+import htl.steyr.snake.Model.Playfield;
+import htl.steyr.snake.View.PlayfieldView;
+
 public class PlayfieldController {
+
+    Playfield snakePlayfield = new Playfield();
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module htl.steyr.snake {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens htl.steyr.snake to javafx.fxml;
     exports htl.steyr.snake;
 }
