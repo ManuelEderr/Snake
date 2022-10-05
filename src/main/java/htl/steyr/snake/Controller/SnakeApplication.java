4683313103c9a8ab730f1715eba58acbb948ff09
nschickm <nschickm@htl-steyr.ac.at>
2022-10-05 08:09:21 +0200

    Update

---
+++ src/main/java/htl/steyr/snake/Controller/SnakeApplication.java
@@ -0,0 +1,65 @@
+package htl.steyr.snake.Controller;
+
+import htl.steyr.snake.Model.ThreadClass1;
+import htl.steyr.snake.View.PlayfieldView;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class SnakeApplication extends Application {
+
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        System.out.println("start");
+
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Splash-view.fxml")));
+        Scene scene = new Scene(pane);
+
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        stage.setTitle("Snake");
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("main");
+/*
+        ThreadClass1 thread = new ThreadClass1("pictures_music\\legendarymusic.mp3");
+        Thread thread2 = new Thread(thread);
+        thread2.start();
+*/
+      //  File file = new File("pictures_music\\legendarymusic.mp3");
+       // Media media = new Media(file.toURI().toString());
+
+
+        /*
+        String path = "pictures_music\\legendarymusic.mp3";
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+*/
+
+        launch();
+
+    }
+}
---
+++ src/main/java/htl/steyr/snake/Controller/SplashController.java
@@ -0,0 +1,73 @@
+package htl.steyr.snake.Controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author nschickm
+ * FXML Controller class
+ * <p>
+ * Die Settings1.fxml Scene wird aufgerufen und dargestellt
+ */
+public class SplashController implements Initializable {
+
+    @FXML
+    private AnchorPane apane;
+    private AnchorPane AnchorPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        splash();
+    }
+
+    /**
+     * Die Settings1.fxml Scene wird nach 2 Sekunden aufgerufen
+     */
+    private void splash() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1999);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            AnchorPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+                            Stage stage = new Stage();
+                            Scene scene = new Scene(AnchorPane);
+                            scene.setFill(Color.TRANSPARENT);
+                            stage.initStyle(StageStyle.TRANSPARENT);
+                            stage.setTitle("Snake");
+                            stage.setScene(scene);
+                            stage.setMinHeight(400);
+                            stage.setMinWidth(255);
+                            stage.show();
+                            apane.getScene().getWindow().hide();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SplashController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                });
+            }
+        }.start();
+    }
+}
